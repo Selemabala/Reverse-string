97d1af57c387fdcb3468222f4411cd63f7b79f1c
Seleman Pharles Mabala <131754068+Selemabala@users.noreply.github.com>
2024-03-05 13:16:16 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Reverse;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, write any word");
+        string userInput = Console.ReadLine();
+        Console.WriteLine($"You wrote the word {userInput}");
+        int numberOfCharacters = userInput.Length;
+        string reversedWord = "";
+        for (int count = numberOfCharacters - 1; count >= 0; count--)
+        {
+            reversedWord = reversedWord + userInput[count];
+        }
+        Console.WriteLine($"The reversed word is {reversedWord}");
+    }
+}
+
